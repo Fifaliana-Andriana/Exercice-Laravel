15c7102c65b21f1e0ab09eb225aa6611a809202f
Fifaliana Andriana <andrianjafyfifaliana@gmail.com>
2026-06-05 11:57:41 +0300

    create table todo

---
+++ database/migrations/2026_06_05_083909_create_to_dos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('Title');
+            $table->text('Content');
+            $table->boolean('is_finished')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('to_dos');
+    }
+};
